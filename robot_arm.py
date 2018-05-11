77686e6db010a1b4f02b996a5c123fcb303874cb
superpingu <arnaudbonetti@me.com>
2018-05-11 15:45:52 +0200

    robot-arm

--- robot_arm.py
+++ robot_arm.py
@@ -13,15 +13,21 @@ r.add_object(AX12(25), "z_axis")
 r.add_object(AX12(142), "y_axis")
 r.add_object(AX12(121), "claw")
 
+claw_position = 0
+r.z_axis.turn(0)
+r.y_axis.turn(0)
+r.claw.turn(0)
+
 while True:
-    event = get_mouse()
+    events = get_mouse()
     for event in events:
-        try:
-            if event.code == REL_WHEEL:
-                r.claw.turn(event.state)
-            elif event.code == REL_X:
-                r.z_axis.turn(event.state)
-            elif event.code == REL_Y:
-                r.axis.turn(event.state)
+	try:
+            if event.code == 'REL_WHEEL':
+                claw_position += 10*event.state
+		r.claw.move(claw_position)
+            elif event.code == 'REL_X':
+                r.z_axis.turn(-event.state)
+            elif event.code == 'REL_Y':
+                r.y_axis.turn(event.state)
         except:
             pass
