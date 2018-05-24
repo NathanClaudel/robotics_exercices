51a69f7e1080782ea988b1990ff938dedf16de20
CLAUDEL Nathan <nathan.claudel@telecom-paristech.fr>
2018-05-24 18:02:30 +0200

    wip

--- basic_example_with_new_syntax.py
+++ basic_example_with_new_syntax.py
@@ -21,7 +21,11 @@ def follow_scroll():
     while 1:
         events = get_mouse()
         for value in [event.state for event in events if event.code == "REL_Y"]:
-            position = (position + value) % 150
+            position = position + value//2
+            if position > 150:
+                position = 150
+            if position < 0:
+                position = 0
             print(position)
             try:
                 r.motor.move(position)
