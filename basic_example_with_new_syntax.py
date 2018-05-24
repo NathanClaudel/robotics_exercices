0aa422f6ae2b0c03ecf09ff956ea5a163bb386b6
CLAUDEL Nathan <nathan.claudel@telecom-paristech.fr>
2018-05-24 17:24:47 +0200

    ajout de la structure du code python

---
+++ basic_example_with_new_syntax.py
@@ -0,0 +1,52 @@
+#import the library
+
+from robot import Robot
+from AX12 import AX12
+
+
+# ---------------------------   ROBOT CONSTRUCTION   --------------------------#
+#creates the robot skeleton
+r = Robot()
+
+#adds motors to the robot skeleton
+r.add_object(AX12(25), "motor")
+
+
+# -------------------------   SEQUENCE DEFINITION ----------------------------#
+
+#defines a sequence of actions
+#note that the sequence is only defined and not run (for the moment)
+r.add_sequence("seq_1")
+
+#first block of actions; all actions of a block are performed simultaneously
+r.add_parallel(r.motor.move, [100])
+#the "wait" defines the end of the block
+#and it waits :
+# 	until max_delay seconds are elapsed
+# 	OR until n_callbacks actions of this block are done
+r.wait(max_delay=3, n_callbacks=2)
+
+
+#second block of actions; this block will be run AFTER the first one
+r.add_parallel(r.motor.turn, [100], False)
+r.wait(max_delay=5, n_callbacks=1)
+
+r.add_parallel(r.motor.turn, [0], False)
+r.wait()
+#you MUST specify where the definition of the sequence ends
+# so the following line means "end of seq_1 definition"
+r.sequence_done()
+
+
+
+# --------------------- RUNNING ! ------------------------------------------#
+
+# We run the sequence we defined above
+r.start_sequence("seq_1")
+
+
+# We wait the end of the sequence execution
+r.wait_sequence()
+
+#close the thread
+r.stop()
