e5fb65e03cbd624b5dc29d330b3b22a3b5e956ab
CLAUDEL Nathan <nathan.claudel@telecom-paristech.fr>
2018-05-24 17:32:59 +0200

    included the input test

---
+++ inputs.py
@@ -0,0 +1,12 @@
+from inputs import devices
+
+for device in devices:
+
+    print(device)
+
+
+from inputs import get_mouse
+while 1:
+     events = get_mouse()
+     for event in events:
+        print(event.ev_type, event.code, event.state)
