fdbd2cdc528afa01f643f158e2e64ce5e88c4b5f
Nathan Claudel <nathan.claudel@telecom-paristech.fr>
2018-10-06 12:09:07 +0200

    WIP

--- evdev_test.py
+++ evdev_test.py
@@ -1,14 +1,19 @@
 #import evdev
 from evdev import InputDevice, categorize, ecodes
+from AX12 import AX12
+
+motor = AX12(172)
 
 #creates object 'gamepad' to store the data
 #you can call it whatever you like
-gamepad = InputDevice('/dev/input/event17')
+gamepad = InputDevice('/dev/input/event0')
 
 #prints out device info at start
 print(gamepad)
 
 #evdev takes care of polling the controller in a loop
 for event in gamepad.read_loop():
-    print(str(categorize(event)).split()[-1], event.value)
+    type, value = str(categorize(event)).split()[-1], event.value
+    if(type == 'ABS_X'):
+        motor.turn(value/300)
     
