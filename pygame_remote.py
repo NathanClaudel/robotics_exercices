d79e7c695aae4de1486cc0b6aac6217d6fd4a336
Nathan Claudel <nathan.claudel@telecom-paristech.fr>
2018-10-07 11:58:46 +0200

    WIP

--- pygame_remote.py
+++ pygame_remote.py
@@ -1,18 +1,21 @@
 import pygame
 import time
+from AX12 import AX12
 
 pygame.init()
 pygame.display.set_mode((300, 200))
 pygame.display.set_caption('robot remote')
+
+wheel = AX12(171)
 
 while True:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                print "left"
+                wheel.turn(20)
             if event.key == pygame.K_RIGHT:
-                print "right"
+                wheel.turn(0)
             if event.key == pygame.K_UP:
                 print "up"
             if event.key == pygame.K_DOWN:
