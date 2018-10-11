92249f55b311f509cd142b25b3ef22bcb055f049
root <root@PC-jeudine.localdomain>
2018-10-11 22:20:37 +0200

    test lance

--- pygame_remote.py
+++ pygame_remote.py
@@ -8,25 +8,30 @@ pygame.display.set_caption('robot remote')
 
 wheel = AX12(172)
 direction=AX12(25)
+l=AX12(142)
 
 while True:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                direction.move(-70)
+                direction.move(-69)
             if event.key == pygame.K_RIGHT:
-                direction.move(-30)
+                direction.move(-29)
             if event.key == pygame.K_UP:
                 wheel.turn(-50)
             if event.key == pygame.K_DOWN:
                 wheel.turn(50)
+            if event.key == pygame.K_P0:
+                l=move(20)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
                 wheel.turn(0)
             if event.key == pygame.K_DOWN:
                 wheel.turn(0)
             if event.key == pygame.K_LEFT:
-                direction.move(-50)
+                direction.move(-49)
             if event.key == pygame.K_RIGHT:
-                direction.move(-50)
+                direction.move(-49)
+            if event.key == pygame.K_P0:
+                l=move(0)
